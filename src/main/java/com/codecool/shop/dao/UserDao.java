ca5d9a818dbbb65235fffc9e03c7f413c9073bf9
max <copita.maxim.info@gmail.com>
2022-11-30 13:39:45 +0200

    Create a user class and user dao interface

---
+++ src/main/java/com/codecool/shop/dao/UserDao.java
@@ -0,0 +1,9 @@
+package com.codecool.shop.dao;
+
+import com.codecool.shop.model.User;
+
+public interface UserDao {
+    void add(User user);
+    User find(String email);
+    String check(String email);
+}
---
+++ src/main/java/com/codecool/shop/model/User.java
@@ -0,0 +1,25 @@
+package com.codecool.shop.model;
+
+public class User {
+    private final String name;
+    private final String password;
+    private final String email;
+
+    public User(String name, String password, String email) {
+        this.name = name;
+        this.password = password;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
