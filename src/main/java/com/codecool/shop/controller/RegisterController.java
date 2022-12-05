f0653b62b35e1f3fa06d25cde053a22bb8e39919
max <copita.maxim.info@gmail.com>
2022-12-05 14:52:57 +0200

    Create controller for register page

---
+++ src/main/java/com/codecool/shop/controller/RegisterController.java
@@ -0,0 +1,60 @@
+package com.codecool.shop.controller;
+
+import com.codecool.shop.config.TemplateEngineUtil;
+import com.codecool.shop.dao.DatabaseManager;
+import com.codecool.shop.model.User;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.SQLException;
+
+
+@WebServlet(name = "RegisterServlet", urlPatterns = {"/register"}, loadOnStartup = 5)
+public class RegisterController extends HttpServlet {
+DatabaseManager databaseManager = new DatabaseManager();
+
+    public RegisterController() throws IOException {
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+//        String username = req.getParameter("username");
+//        String email = req.getParameter("email");
+//        String password = req.getParameter("psw");
+//        User user = new User(username, password, email);
+//        databaseManager.addNewUser(user);
+        engine.process("product/register.html",context,resp.getWriter());
+
+
+//        User user = new User();
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
+        WebContext context = new WebContext(req, resp, req.getServletContext());
+
+        String username = req.getParameter("username");
+        String email = req.getParameter("email");
+        String password = req.getParameter("psw");
+        User user = new User(username, password, email);
+        try {
+            databaseManager.setup();
+            databaseManager.addNewUser(user);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        engine.process("product/index.html",context,resp.getWriter());
+
+    }
+}
