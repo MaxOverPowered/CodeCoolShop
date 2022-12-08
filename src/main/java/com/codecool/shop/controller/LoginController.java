19dcd3c80db3a680fcab9c782cd38e6d0c1ae91c
max <copita.maxim.info@gmail.com>
2022-12-08 11:44:36 +0200

    get products for logged user

--- src/main/java/com/codecool/shop/controller/LoginController.java
+++ src/main/java/com/codecool/shop/controller/LoginController.java
@@ -2,6 +2,12 @@ package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
 import com.codecool.shop.dao.DatabaseManager;
+import com.codecool.shop.dao.ProductCategoryDao;
+import com.codecool.shop.dao.ProductDao;
+import com.codecool.shop.dao.SupplierDao;
+import com.codecool.shop.dao.implementation.ProductCategoryJdbc;
+import com.codecool.shop.dao.implementation.ProductDaoJdbc;
+import com.codecool.shop.dao.implementation.SupplierDaoJdbc;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
@@ -33,6 +39,9 @@ DatabaseManager databaseManager=new DatabaseManager();
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(req.getServletContext());
         WebContext context = new WebContext(req, resp, req.getServletContext());
+        ProductDao productDataStore = ProductDaoJdbc.getInstance();
+        ProductCategoryDao productCategoryDataStore = ProductCategoryJdbc.getInstance();
+        SupplierDao supplierDao = SupplierDaoJdbc.getInstance();
 
         String username = req.getParameter("username");
         String password = req.getParameter("psw");
@@ -40,6 +49,9 @@ DatabaseManager databaseManager=new DatabaseManager();
             databaseManager.setup();
             databaseManager.checkIfUserExist(username,password);
             context.setVariable("username", username);
+            context.setVariable("suppliers", supplierDao.getAll());
+            context.setVariable("products", productDataStore.getAll());
+            context.setVariable("categories", productCategoryDataStore.getAll());
 
         } catch (SQLException e) {
             throw new RuntimeException(e);
