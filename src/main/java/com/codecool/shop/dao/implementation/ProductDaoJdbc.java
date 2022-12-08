8eb38c185b2cc435feb56cca4b9707d379c388a6
max <copita.maxim.info@gmail.com>
2022-12-08 11:26:45 +0200

    get list with products by suppliers parameter

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -63,7 +63,23 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public List<Product> getBy(Supplier supplier) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT products.product_id,products.product_name, products.product_description, products.product_defaultprice, products.product_currency,cat.category_name,products.product_picture,s.suplier_name FROM products JOIN category cat on cat.category_id = products.product_category_id JOIN supplier s on s.suplier_id = products.product_supplier_id WHERE s.suplier_name = ?;";
+            PreparedStatement searchBy = conn.prepareStatement(sql);
+            searchBy.setString(1,supplier.getName());
+            ResultSet rs = searchBy.executeQuery();
+
+            List<Product> result = new ArrayList<>();
+            while (rs.next()) {
+                Product product = new Product(rs.getString(2), new BigDecimal(rs.getInt(4)), rs.getString(5), rs.getString(3), new ProductCategory(rs.getString(6), null), new Supplier(rs.getString(8)), rs.getString(7));
+                result.add(product);
+            }
+//            logger.info("Successfully found all products by supplier");
+            return result;
+        } catch (SQLException e) {
+//            logger.warn("Runtime exception was thrown");
+            throw new RuntimeException("Error while reading all products", e);
+        }
     }
 
     @Override
