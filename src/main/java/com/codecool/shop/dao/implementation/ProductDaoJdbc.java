b6ec2f40cfbe70e5e80a9edda2c0a2705638bd0a
max <copita.maxim.info@gmail.com>
2022-12-08 10:50:49 +0200

    method return list with products by category

--- src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductDaoJdbc.java
@@ -51,7 +51,7 @@ public class ProductDaoJdbc implements ProductDao {
             List<Product> result = new ArrayList<>();
             if (!rs.next()) return null;
             while (rs.next()) {
-                Product product = new Product(rs.getString(1), new BigDecimal(rs.getFloat(2)), rs.getString(3), rs.getString(4), new ProductCategory(rs.getString(5), rs.getString(6)), new Supplier(rs.getString(7)),rs.getString(8));
+                Product product = new Product(rs.getString(1), new BigDecimal(rs.getFloat(2)), rs.getString(3), rs.getString(4), new ProductCategory(rs.getString(5), rs.getString(6)), new Supplier(rs.getString(7)), rs.getString(8));
                 System.out.println(product);
                 result.add(product);
             }
@@ -68,6 +68,24 @@ public class ProductDaoJdbc implements ProductDao {
 
     @Override
     public List<Product> getBy(ProductCategory productCategory) {
-        return null;
+        try (Connection conn = dataSource.getConnection()) {
+            String sql = "SELECT products.product_id,products.product_name, products.product_description, products.product_defaultprice, products.product_currency,p.category_name,products.product_picture,s.suplier_name FROM products JOIN category p on p.category_id = products.product_category_id JOIN supplier s on s.suplier_id = products.product_supplier_id WHERE category_name = ?;";
+            PreparedStatement searchBy = conn.prepareStatement(sql);
+            searchBy.setString(1, productCategory.getName());
+            ResultSet rs = searchBy.executeQuery();
+            List<Product> result = new ArrayList<>();
+            while (rs.next()) {
+                Product product = new Product(rs.getString(2), new BigDecimal(rs.getInt(4)), rs.getString(5), rs.getString(3), new ProductCategory(rs.getString(6), null), new Supplier(rs.getString(8)), rs.getString(7));
+                ;
+                product.setId(rs.getInt("product_id"));
+                result.add(product);
+            }
+//            logger.info("Successfully found all products by category");
+            return result;
+        } catch (SQLException e) {
+//            logger.warn("Runtime exception was thrown");
+            throw new RuntimeException("Error while reading all products", e);
+        }
     }
+
 }
